a1db12d21a84733374b077181da92a22b7ac6c62
zengtai <zengtai@gmail.com>
2022-07-09 18:34:51 +0800

    添加统计

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,71 @@
+import Script from "next/script";
+import Head from "next/head";
+
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { GA_ID } from "../lib/constants";
+
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`);
+      //  NProgress.start();
+    };
+    const handleStop = (url) => {
+      console.log(`${url}`);
+      //  NProgress.done();
+    };
+    const handleDone = (url) => {
+      gtag.pageview(url);
+      //  NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleDone);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleDone);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router.events]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width, height=device-height"
+        />
+      </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
